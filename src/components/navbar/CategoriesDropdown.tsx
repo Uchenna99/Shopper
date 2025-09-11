08ea89a745f8ec61763166bce4ba6da9403776f1
Uchenna99 <uchennaagbu@gmail.com>
2025-09-11 23:35:13 +0100

    adjustments

--- src/components/navbar/CategoriesDropdown.tsx
+++ src/components/navbar/CategoriesDropdown.tsx
@@ -1,31 +1,24 @@
-import {  useState } from "react";
 import { useAppContext } from "../../hooks/AppContext";
+import CategoryOption from "./CategoryOption";
 
 
 const CategoriesDropdown = () => {
   const {  } = useAppContext();
-  const [hovering, setHovering] = useState(false);
 
 
   return (
-    <div className="w-[600px] bg-white p-10 shadow-sm rounded-md absolute top-7
-      grid grid-cols-1 sm:grid-cols-2"
+    <div className="w-[690px] bg-white py-10 px-5 shadow-sm rounded-md absolute top-7
+      grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-5"
       >
         
-      <div className="flex items-center gap-2 bg-[#f5f6f6] p-1 rounded-sm cursor-pointer"
-        onMouseEnter={()=> setHovering(true)} onMouseLeave={()=> setHovering(false)}>
+      <CategoryOption title="Men's Clothing" amount={20}/>
 
-        <div className="w-14 h-14 min-w-14 min-h-14 bg-gray-200 rounded-sm"></div>
-        <div className="flex flex-col gap-0">
-          <p className={`font-monts-semi-bold ${hovering? 'text-orange-400':'text-black-text'} transition-all duration-200`}>
-            Men's Clothing
-          </p>
-          <p className="text-black-text text-sm font-monts-regular">
-            20 items available
-          </p>
-        </div>
+      <CategoryOption title="Women's Clothing" amount={25}/>
 
-      </div>
+      <CategoryOption title="Children's Clothing" amount={10}/>
+
+      <CategoryOption title="Accessories" amount={1}/>
+      
 
     </div>
   )
---
+++ src/components/navbar/CategoryOption.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+interface Props {
+    title: string;
+    amount: number;
+}
+
+const CategoryOption = ({ title, amount }:Props) => {
+    const [hovering, setHovering] = useState(false);
+
+  return (
+    <div className="flex items-center gap-2 bg-[#f5f6f6] p-1 rounded-sm cursor-pointer"
+        onMouseEnter={()=> setHovering(true)} onMouseLeave={()=> setHovering(false)}>
+
+        <div className="w-14 h-14 min-w-14 min-h-14 bg-gray-200 rounded-sm"></div>
+        <div className="flex flex-col gap-0">
+          <p className={`font-monts-semi-bold ${hovering? 'text-orange-400':'text-black-text'} transition-all duration-200`}>
+            {title}
+          </p>
+          <p className="text-black-text text-sm font-monts-regular">
+            {amount} {amount < 2? 'item':'items'} available
+          </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default CategoryOption;
