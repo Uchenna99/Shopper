dadb76510d079ae1b78b07b5bda9d05cc81bd28f
Uchenna99 <uchennaagbu@gmail.com>
2025-09-14 22:55:47 +0100

    our services added

--- src/components/ServicesCard.tsx
+++ src/components/ServicesCard.tsx
@@ -14,7 +14,7 @@ const ServicesCard = ({ title, text, image }:Props) => {
     <div className="w-full aspect-[2/2.6] bg-gray-100 rounded-xl overflow-hidden cursor-pointer"
         onMouseEnter={()=>setHovering(true)} onMouseLeave={()=>setHovering(false)}>
 
-        <div className="w-full h-2/5 flex flex-col gap-3 p-10">
+        <div className="w-full h-2/5 flex flex-col gap-3 p-10 sm:p-5 lg:p-10">
             <h4 className="text-black-text text-xl font-monts-bold">
                 {title}
             </h4>
--- src/components/homepage/OurServices.tsx
+++ src/components/homepage/OurServices.tsx
@@ -14,7 +14,7 @@ const OurServices = () => {
                 </h1>
 
                 
-                <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-5">
+                <div className="w-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5">
                     {
                         services.map((service, index)=>(
                             <ServicesCard
