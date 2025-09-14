a55d565c2cb32debed43dd193b58eff8a3895ee1
Uchenna99 <uchennaagbu@gmail.com>
2025-09-14 23:30:59 +0100

    cash back adjusted

--- src/components/homepage/CashBack.tsx
+++ src/components/homepage/CashBack.tsx
@@ -4,11 +4,11 @@ import HeroButton from "../buttons/HeroButton";
 
 const CashBack = () => {
   return (
-    <div className="w-full h-[400px] flex justify-center bg-orange-100 overflow-hidden">
-        <div className="w-[1300px] flex items-center px-5 gap-10">
+    <div className="w-full h-[300px] md:h-[400px] flex justify-center bg-orange-100 overflow-hidden">
+        <div className="w-[1300px] flex items-center md:gap-10 relative">
 
-            <div className="flex flex-col gap-5 py-15">
-                <h1 className="text-5xl text-black-text font-merienda-bold">
+            <div className="flex flex-col gap-5 bg-white/50 rounded-2xl max-xl:rounded-tl-none max-xl:rounded-bl-none p-5  z-30">
+                <h1 className="text-3xl md:text-4xl lg:text-5xl text-black-text font-merienda-bold leading-normal">
                     Get 5% Cash Back
                 </h1>
                 <p className="text-black-text font-monts-medium">
@@ -19,9 +19,9 @@ const CashBack = () => {
             </div>
 
 
-            <div className="w-fit h-full relative bg-red-300">
+            <div className="w-[20px] h-full absolute left-1/4">
                 <img src="https://res.cloudinary.com/df6xz7bqp/image/upload/v1757869692/14276-removebg-preview-min_erlvzd.png" alt="happy lady" 
-                    className="absolute bottom-0 max-h-none max-w-none"
+                    className="absolute bottom-0 max-md:w-[400px] max-h-none max-w-none"
                 />
             </div>
 
