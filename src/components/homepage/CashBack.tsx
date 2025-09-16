e24d04ff4e37d2b487a7c27cca5f24c978302edb
Uchenna99 <uchennaagbu@gmail.com>
2025-09-16 21:22:16 +0100

    adding animations

--- src/components/homepage/CashBack.tsx
+++ src/components/homepage/CashBack.tsx
@@ -14,7 +14,7 @@ const CashBack = () => {
 
             <motion.div className="flex flex-col gap-5 bg-white/50 rounded-2xl max-xl:rounded-tl-none max-xl:rounded-bl-none p-5  z-30"
                 initial={{opacity:0, x:'-110%'}}
-                animate={inView? {opacity:1, x:'0%'}:{}}
+                animate={inView? {opacity:1, x:0}:{}}
                 transition={{duration:0.5, ease:'easeInOut'}}>
                 <h1 className="text-3xl md:text-4xl lg:text-5xl text-black-text font-merienda-bold leading-normal">
                     Get 5% Cash Back
@@ -27,9 +27,12 @@ const CashBack = () => {
             </motion.div>
 
 
-            <div className="w-[20px] h-full absolute left-1/4">
-                <img src="https://res.cloudinary.com/df6xz7bqp/image/upload/v1757869692/14276-removebg-preview-min_erlvzd.png" alt="happy lady" 
+            <div className="w-[20px] h-full absolute left-1/3">
+                <motion.img src="https://res.cloudinary.com/df6xz7bqp/image/upload/v1757869692/14276-removebg-preview-min_erlvzd.png" alt="happy lady" 
                     className="absolute bottom-0 max-md:w-[400px] max-h-none max-w-none"
+                    initial={{y:'100%', opacity:0}}
+                    animate={inView? {y:0, opacity:1}:{}}
+                    transition={{duration:0.7, delay:0.4, ease:'easeInOut'}}
                 />
             </div>
 
