6b2bb23b3c79fae03f4c68d4a303255101ebaf3b
Uchenna99 <uchennaagbu@gmail.com>
2025-09-18 21:52:55 +0100

    hero swiperjs

--- src/components/buttons/HeroButton.tsx
+++ src/components/buttons/HeroButton.tsx
@@ -4,9 +4,9 @@ interface Props {
 
 const HeroButton = ({title}:Props) => {
   return (
-    <button className="w-fit border-none outline-none rounded-4xl bg-orange-500 text-nowrap text-lg text-white font-monts-medium px-7 py-3 
-      transition-all duration-300 cursor-pointer hover:bg-orange-400 hover:shadow-md hover:scale-103
-      active:bg-orange-400 active:scale-95">
+    <button className="w-fit border-none outline-none rounded-4xl bg-gray-900 text-nowrap text-lg text-white font-monts-medium px-7 py-3 
+      transition-all duration-300 cursor-pointer hover:bg-gray-800 hover:shadow-md hover:scale-103
+      active:bg-gray-800 active:scale-95">
 
         {title}
     </button>
