6faa1648d8a41e6a2a45f6fe9d003cd412f366cf
Uchenna99 <uchennaagbu@gmail.com>
2025-09-18 23:53:38 +0100

    men's section

--- src/components/categories/MensCategory.tsx
+++ src/components/categories/MensCategory.tsx
@@ -14,7 +14,7 @@ const MensCategory = () => {
           <NavigationBar/>
 
           <div className="w-full h-[300px] flex justify-center">
-            <div className="w-[1300px] h-full bg-orange-200 rounded-sm mt-[2px] flex items-center px-20">
+            <div className="w-[1300px] h-full bg-orange-200 rounded-sm mt-[2px] flex items-center gap-[200px] px-20 relative overflow-hidden">
 
               <div className="flex flex-col gap-5">
                 <h1 className="text-black-text text-4xl font-merienda-bold leading-normal">
@@ -23,6 +23,12 @@ const MensCategory = () => {
 
                 <HeroButton title="Explore"/>
               </div>
+
+              <img 
+                src="https://res.cloudinary.com/df6xz7bqp/image/upload/v1758235659/Adobe_Express_-_file_2_-min_gvfg95.png" 
+                alt="young man" 
+                className="w-[400px] absolute -top-3 right-1/4 rotate-y-180"
+              />
             </div>
           </div>
             
