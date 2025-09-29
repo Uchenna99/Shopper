885f4738e0621129adec8e4974600a16f14633bd
Uchenna99 <uchennaagbu@gmail.com>
2025-09-29 13:42:44 +0100

    adding delivery option

--- src/components/DeliveryInfo.tsx
+++ src/components/DeliveryInfo.tsx
@@ -4,20 +4,43 @@ import InputText from "./InputText";
 
 const DeliveryInfo = () => {
   return (
-    <div className="w-full bg-white rounded-sm shadow-sm pt-4 px-4 border border-gray-300">
+    <div className="w-full bg-white rounded-sm shadow-sm p-4 border border-gray-300 flex flex-col gap-4">
 
-        <div className="w-full flex justify-between items-center gap-2 border-b border-gray-300 pb-2 mb-5">
+        <div className="w-full flex justify-between items-center gap-2 border-b border-gray-300 pb-2 mb-2">
             <p className="text-black-text font-monts-semi-bold">
                 Delivery Information
             </p>
         </div>
 
 
-        <div className="w-full flex flex-col md:flex-row items-center">
-            <div className="flex-1">
-                <InputText title="Name"/>    
+        <div className="w-full flex flex-col md:flex-row items-center gap-4">
+            <div className="flex-1 w-full">
+                <InputText title="First Name" placeHolder="eg. John" required/>    
+            </div> 
+            <div className="flex-1 w-full">
+                <InputText title="Last Name" placeHolder="eg. Wick" required/>    
             </div>    
-        </div>    
+        </div>   
+
+        <InputText title="Address" placeHolder="Enter your address" required/> 
+
+        <div className="w-full flex flex-col md:flex-row items-center gap-4">
+            <div className="flex-1 w-full">
+                <InputText title="City / Town" placeHolder="eg. Port Harcourt" required/>    
+            </div> 
+            <div className="flex-1 w-full">
+                <InputText title="Zip Code" placeHolder="eg. 500272" required/>    
+            </div>    
+        </div>
+
+        <div className="w-full flex flex-col md:flex-row items-center gap-4">
+            <div className="flex-1 w-full">
+                <InputText title="Email" placeHolder="eg. johnwick@continental.com" required/>    
+            </div> 
+            <div className="flex-1 w-full">
+                <InputText title="Phone" placeHolder="eg. +234-7031234567" required/>    
+            </div>    
+        </div>
     </div>
   )
 }
--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -2,19 +2,22 @@
 
 interface Props {
     title: string;
+    placeHolder: string;
+    required?: boolean;
 }
 
-const InputText = ({ title }:Props) => {
+const InputText = ({ title, required, placeHolder }:Props) => {
   return (
-    <div className="w-full flex flex-col gap-2">
+    <div className="w-full flex flex-col gap-1">
 
         <p className="text-black-text text-xs font-monts-medium">
-            {title}
+            {title} {required && '*'}
         </p>
 
 
         <input type="text" 
-            className="w-full h-9 min-h-9"
+            className="w-full h-9 min-h-9 border-none outline-none ring ring-gray-300 rounded-md px-3 text-black-text text-sm"
+            placeholder={placeHolder}
         />
     </div>
   )
